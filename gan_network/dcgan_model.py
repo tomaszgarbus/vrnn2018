b0b9264eaead3724f7546c43f734fd1cfe582de1
Tomasz Garbus <tgarbus@chromium.org>
2019-01-15 21:39:48 +0100

    Minor fix in printing epoch no

--- gan_network/dcgan_model.py
+++ gan_network/dcgan_model.py
@@ -126,7 +126,7 @@ class DCGans:
         batch_count = dataset.shape[0] // batch_size
 
         for i in range(n_epoch):
-            for j in tqdm(range(batch_count), desc="Epoch " + str(n_epoch) if self.verbosity > 0 else "", smoothing=0):
+            for j in tqdm(range(batch_count), desc="Epoch " + str(i) if self.verbosity > 0 else "", smoothing=0):
                 # Input for the generator
                 noise_input = np.random.rand(batch_size, self.input_space_size)
 
@@ -137,7 +137,7 @@ class DCGans:
                 # these are the predicted images from the generator
                 predictions = self.generator.predict(noise_input, batch_size=batch_size)
 
-                if j % 10 == 0:
+                if j % 100 == 0:
                     show_images(predictions)
 
                 # the discriminator takes in the real images and the generated images
