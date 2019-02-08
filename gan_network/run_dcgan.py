8f5aa7e3d19703efaf1ffb912faa5f763f036772
Tomasz Garbus <tgarbus@chromium.org>
2019-02-08 18:33:21 +0100

    model to try

--- gan_network/run_dcgan.py
+++ gan_network/run_dcgan.py
@@ -2,14 +2,21 @@ from gan_network.dcgan_model import  DCGans
 from gan_network.preprocess import Preprocess
 from tools.visualise import show_images
 import numpy as np
+import gan_network.inception_score as IS
 
 
 if __name__ == '__main__':
     preprocess = Preprocess()
-    dataset = preprocess.load_dataset()
+    dataset = preprocess.load_dataset() * 255
 
     sample_dataset = dataset[np.random.randint(0, dataset.shape[0], size=9)]
     show_images(sample_dataset)
+
+    dataset_imgs = []
+    for i in range(dataset.shape[0]):
+        dataset_imgs.append(dataset[i])
+    print(IS.get_inception_score(dataset_imgs))
+    exit(0)
 
     model = DCGans()
     model.fit(dataset, n_epoch=100, batch_size=64)
